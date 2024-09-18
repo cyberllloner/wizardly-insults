4454e5ec1d80cbdec6621b09de25cdfbd4b1b7d5
cyberllloner <cyberllloner@proton.me>
2024-09-18 15:48:01 +0330

    Initial commit

--- wizardly-insults.py
+++ wizardly-insults.py
@@ -7,3 +7,23 @@ def generate_random_insult():
     else:
         random_insult = response.text
         return random_insult
+
+def add_text_border(text):
+    dash = len(text) * "-"
+    transformed_text = dash + "\n" + text + "\n" + dash
+    return transformed_text
+
+def main():
+    text = add_text_border(generate_random_insult())
+    wizard_ascii_art = r'''
+    (\.   \      ,/)
+    \(   |\     )/
+    //\  | \   /\\
+    (/ /\_#oo#_/\ \)
+    \/\  ####  /\/
+        `##'
+    '''
+    result = text + "\n" + wizard_ascii_art
+    print(result)
+
+main()
